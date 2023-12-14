768c358ebcb0e0c717121accb1f985f1847010de
z-e-a <e.a.zubkov@yandex.ru>
2023-12-14 12:28:47 +0300

    feat: solve task 'vigenere-cipher'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect) {
+    if (isDirect == false) {
+      this.isReverse = true;
+    } else {
+      this.isReverse = false;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (arguments.length < 2) {
+      throw new Error('Incorrect arguments!')
+    }
+    if ((message === undefined) || (key === undefined)) {
+      throw new Error('Incorrect arguments!')
+    }
+    let factor = message.length / key.length;
+    let keyStr = key.repeat(Math.ceil(factor)).slice(0, message.length).toUpperCase();
+    message = message.toUpperCase();
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let outStr = '';
+
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let index = alphabet.indexOf(message[i], 0) + alphabet.indexOf(keyStr[i], 0);
+        index %= 26;
+        outStr += alphabet[index];
+      } else {
+        outStr += message[i];
+        keyStr = keyStr.slice(0, i) + message[i] + keyStr.slice(i);
+      }
+    }
+    if (this.isReverse) {
+      outStr = outStr.split('').reverse().join('');
+    }
+    return outStr;
+  }
+  decrypt(message, key) {
+    if (arguments.length < 2) {
+      throw new Error('Incorrect arguments!')
+    }
+    if ((message === undefined) || (key === undefined)) {
+      throw new Error('Incorrect arguments!')
+    }
+    let factor = message.length / key.length;
+    let keyStr = key.repeat(Math.ceil(factor)).slice(0, message.length).toUpperCase();
+    message = message.toUpperCase();
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let outStr = '';
+
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let index = alphabet.indexOf(message[i], 0) - alphabet.indexOf(keyStr[i], 0) + 26;
+        index %= 26;
+        outStr += alphabet[index];
+      } else {
+        outStr += message[i];
+        keyStr = keyStr.slice(0, i) + message[i] + keyStr.slice(i);
+      }
+    }
+    if (this.isReverse) {
+      outStr = outStr.split('').reverse().join('');
+    }
+    return outStr;
   }
 }
 
